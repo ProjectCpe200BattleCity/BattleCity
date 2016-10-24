a1ac3708a66c7d547b8aed2eeec89f67b58a5c28
kohSatrarin <koh.satrarin@gmail.com>
2016-10-25 01:40:24 +0700

    First version

---
+++ src/iTank.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Guu on 10/19/2016 AD.
+ */
+public interface Tank {
+
+    public Tank append(int row, int col);
+    public int hp();
+    public int addHp();                             //add hp when pick up health
+    public int attack();                            //attack point
+
+
+
+
+}
