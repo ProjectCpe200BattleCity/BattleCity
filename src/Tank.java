4c178ab2574fe7492177f8b2add08866f5761f52
kohSatrarin <koh.satrarin@gmail.com>
2016-10-25 01:49:18 +0700

    First version

--- src/Tank.java
+++ src/Tank.java
@@ -1,14 +1,18 @@
 import java.util.*;
+
 /**
- * Write a description of interface iSnake here.
+ * Write a implements of interface iTank here.
  * Created by SatrarinSaejew on 10/19/2016 AD.
  */
 
-public interface iTank {
+public class Tank implements iTank {
 
+
+    /*
     public iTank append(int row, int col);
     public int hp();
     public int addHp();                             //add hp when pick up health
     public int attack();                            //attack point
+    */
 
 }
